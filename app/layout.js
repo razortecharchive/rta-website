87b8749bcdd19686579829a82bbf2cbb98cb6b60
maringa <maringa@maringanoMacBook-Pro.local>
2026-05-15 20:12:36 +0900

    language switch fix

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import { LangProvider } from "./context/LangContext";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -20,12 +19,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="ja"
+      lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
-        <LangProvider>{children}</LangProvider>
-      </body>
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
