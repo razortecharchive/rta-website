d7401f5987992a053e0cfc2a3f0a460519316780
maringa <maringa@maringanoMacBook-Pro.local>
2026-05-27 15:51:33 +0900

    lang switch nav position

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    qualities: [100],
+    minimumCacheTTL: 60,
+    unoptimized: false,
+    formats: ['image/jpeg'],
+  },
 };
 
 export default nextConfig;
